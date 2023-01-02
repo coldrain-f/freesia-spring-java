8770e456978350747e185f796ae460729161789b
coldrain-f <intmain__@naver.com>
2023-01-02 17:40:17 +0900

    Initial commit

---
+++ src/main/java/edu/coldrain/FreesiaSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.coldrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreesiaSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreesiaSpringJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/coldrain/FreesiaSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coldrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreesiaSpringJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
