a674cea283dab3aa92ae4cee3328e7161699fd35
coldrain-f <intmain__@naver.com>
2023-01-02 18:32:07 +0900

    Feat: [#1] 단어장 Service 생성

---
+++ src/main/java/edu/coldrain/book/service/BookService.java
@@ -0,0 +1,14 @@
+package edu.coldrain.book.service;
+
+import edu.coldrain.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+
+}
