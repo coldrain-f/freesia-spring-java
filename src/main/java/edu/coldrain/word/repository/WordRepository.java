1f241794a774e545e8939ad1d24b2ece7ab0bdba
coldrain-f <intmain__@naver.com>
2023-01-04 23:16:01 +0900

    Fix: [#3] WordRepository T와 ID 순서가 바뀐 문제 해결

--- src/main/java/edu/coldrain/word/repository/WordRepository.java
+++ src/main/java/edu/coldrain/word/repository/WordRepository.java
@@ -3,5 +3,5 @@ package edu.coldrain.word.repository;
 import edu.coldrain.word.entity.WordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WordRepository extends JpaRepository<Long, WordEntity> {
+public interface WordRepository extends JpaRepository<WordEntity, Long> {
 }
