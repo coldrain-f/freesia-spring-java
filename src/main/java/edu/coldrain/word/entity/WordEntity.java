ccabfbb76186cf3e5aae7868a7689fec538749b2
coldrain-f <intmain__@naver.com>
2023-01-04 23:14:14 +0900

    Fix: [#3] 단어 엔티티 이름이 PK로 설정되어 있는 문제 해결

--- src/main/java/edu/coldrain/word/entity/WordEntity.java
+++ src/main/java/edu/coldrain/word/entity/WordEntity.java
@@ -1,19 +1,22 @@
 package edu.coldrain.word.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WordEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+
+    @Column()
     private String name;
 
     @Builder
