d8288467ce46cf20b97e47970999f9b084107fc6
coldrain-f <intmain__@naver.com>
2023-01-05 21:28:25 +0900

    Feat: [#10] 스프링 시큐리티 CSRF disable 설정

---
+++ src/main/java/edu/coldrain/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package edu.coldrain.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf()
+                .disable()
+                .build();
+    }
+}
