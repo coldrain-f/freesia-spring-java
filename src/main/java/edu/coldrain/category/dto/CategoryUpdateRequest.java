bfb995b159e9e64eae9c397f18a845fc78dcb6b5
coldrain-f <intmain__@naver.com>
2023-01-08 18:08:38 +0900

    Chore: [#2] 카테고리 수정 DTO에 기본 생성자 추가

--- src/main/java/edu/coldrain/category/dto/CategoryUpdateRequest.java
+++ src/main/java/edu/coldrain/category/dto/CategoryUpdateRequest.java
@@ -1,11 +1,14 @@
 package edu.coldrain.category.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CategoryUpdateRequest {
 
-    private final String name;
+    private String name;
 
     public CategoryUpdateRequest(final String name) {
         this.name = name;
