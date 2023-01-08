eb7d00581298a31faeb5144b0a4be140694b329c
coldrain-f <intmain__@naver.com>
2023-01-08 19:23:48 +0900

    Feat: [#17] 단어장 서비스 클래스 레벨에 ＠Transactional(readOnly = true) 적용

--- src/main/java/edu/coldrain/book/service/BookService.java
+++ src/main/java/edu/coldrain/book/service/BookService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class BookService {
 
     private final BookRepository bookRepository;
