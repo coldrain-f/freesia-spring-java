5d6520068bfb55ecb779290c9eeeee3e8ec2bb36
coldrain-f <intmain__@naver.com>
2023-01-08 21:17:36 +0900

    Feat: [#10] 스프링 시큐리티 h2-console 인증 면제 설정

--- src/main/java/edu/coldrain/security/SecurityConfig.java
+++ src/main/java/edu/coldrain/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,9 +12,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf()
                 .disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .build();
     }
 }
