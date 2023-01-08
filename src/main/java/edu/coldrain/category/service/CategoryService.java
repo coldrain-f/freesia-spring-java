6c9e1b5d6491db257002ba163bacd988bbf06e09
coldrain-f <intmain__@naver.com>
2023-01-08 19:21:43 +0900

    Feat: [#14] 카테고리 서비스 클래스 레벨에 ＠Transactional(readOnly = true) 적용

--- src/main/java/edu/coldrain/category/service/CategoryService.java
+++ src/main/java/edu/coldrain/category/service/CategoryService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
