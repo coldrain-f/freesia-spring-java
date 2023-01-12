3ba62a03effb6a18ed57ef56508a7a0d86f8106b
coldrain-f <intmain__@naver.com>
2023-01-12 20:51:34 +0900

    Feat: [#17] 단어장 수정 DTO에 Bean Validation 적용

--- src/main/java/edu/coldrain/dto/BookUpdateRequest.java
+++ src/main/java/edu/coldrain/dto/BookUpdateRequest.java
@@ -3,13 +3,19 @@ package edu.coldrain.dto;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @ToString
 public class BookUpdateRequest {
 
+    @NotNull
     private final String name;
+    @NotNull
     private final String content;
+    @NotNull
     private final String language;
+    @NotNull
     private final String shareStatus;
 
     public BookUpdateRequest(final String name, final String content, final String language, final String shareStatus) {
