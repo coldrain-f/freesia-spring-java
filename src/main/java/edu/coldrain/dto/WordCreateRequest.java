d951fb1aa1c33617ad5f348919057c6d64db0b8d
coldrain-f <intmain__@naver.com>
2023-01-12 20:53:58 +0900

    Feat: [#20] 단어 등록, 수정 DTO에 Bean Validation 적용

--- src/main/java/edu/coldrain/dto/WordCreateRequest.java
+++ src/main/java/edu/coldrain/dto/WordCreateRequest.java
@@ -3,9 +3,12 @@ package edu.coldrain.dto;
 import edu.coldrain.entity.Word;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class WordCreateRequest {
 
+    @NotNull
     private final String name;
 
     public WordCreateRequest(final String name) {
--- src/main/java/edu/coldrain/dto/WordUpdateRequest.java
+++ src/main/java/edu/coldrain/dto/WordUpdateRequest.java
@@ -2,9 +2,12 @@ package edu.coldrain.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class WordUpdateRequest {
 
+    @NotNull
     private final String name;
 
     public WordUpdateRequest(final String name) {
