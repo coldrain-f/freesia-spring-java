0d8a2975c8acbef7a59a388be7c5ed969c38b475
coldrain-f <intmain__@naver.com>
2023-01-13 23:53:05 +0900

    Test: [#7] 단어장 삭제 테스트 코드 작성

--- src/test/java/edu/coldrain/repository/BookRepositoryTest.java
+++ src/test/java/edu/coldrain/repository/BookRepositoryTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 @DataJpaTest
@@ -16,7 +19,7 @@ class BookRepositoryTest {
     private BookRepository bookRepository;
 
     @Test
-    @DisplayName("단어장이 하나 등록된다.")
+    @DisplayName("단어장을 하나 등록한다.")
     void save() {
         // given - 데이터 준비
         Book book = Book.builder()
@@ -34,5 +37,26 @@ class BookRepositoryTest {
         assertEquals(book.getContent(), savedBook.getContent());
         assertEquals(book.getLanguage(), savedBook.getLanguage());
         assertEquals(book.getShareStatus(), savedBook.getShareStatus());
+    }
+
+    @Test
+    @DisplayName("단어장을 하나 삭제한다.")
+    void delete() {
+        // given
+        Book book = Book.builder()
+                .name("단어가 읽기다 기본편")
+                .content("단어가 읽기다 기본편 - 내용")
+                .language("en")
+                .shareStatus("public")
+                .build();
+
+        Long savedBookId = bookRepository.save(book).getId();
+
+        // when
+        bookRepository.deleteById(savedBookId);
+        Optional<Book> findBook = bookRepository.findById(savedBookId);
+
+        // then
+        assertFalse(findBook.isPresent());
     }
 }
