38170e67cd71673c6dd4418b72b6bbad1144903e
coldrain-f <intmain__@naver.com>
2023-01-14 03:29:03 +0900

    Feat: [#27] User Entity ALL Args 생성자 및 변경 메서드 추가 작성

--- src/main/java/edu/coldrain/entity/User.java
+++ src/main/java/edu/coldrain/entity/User.java
@@ -2,6 +2,7 @@ package edu.coldrain.entity;
 
 import edu.coldrain.common.Timestamped;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +26,20 @@ public class User extends Timestamped {
     private String email;
 
     private String role;
+
+    @Builder
+    public User(final String username, final String password, final String email, final String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public void changePassword(final String password) {
+        this.password = password;
+    }
+
+    public void changeRole(final String role) {
+        this.role = role;
+    }
 }
