a16649f0729fa34541cbfdadc7b8473864133954
coldrain-f <intmain__@naver.com>
2023-01-15 18:06:01 +0900

    Chore: [#28] 스프링 시큐리티 설정파일 패키지 이동
    - security 패키지 → config 패키지로 이동

--- src/main/java/edu/coldrain/config/SecurityConfig.java
+++ src/main/java/edu/coldrain/config/SecurityConfig.java
@@ -1,7 +1,8 @@
-package edu.coldrain.security;
+package edu.coldrain.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
