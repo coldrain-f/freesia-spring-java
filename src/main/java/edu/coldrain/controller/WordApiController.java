5dd176d8eac57efe450bcf99e69ba7fde3a708f0
coldrain-f <intmain__@naver.com>
2023-01-15 23:45:16 +0900

    Feat: [#29] 단어 API 엔드포인트 일괄 변경

--- src/main/java/edu/coldrain/controller/WordApiController.java
+++ src/main/java/edu/coldrain/controller/WordApiController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class WordApiController {
 
     private final WordService wordService;
