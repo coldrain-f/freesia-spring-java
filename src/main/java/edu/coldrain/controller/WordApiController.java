0ee05684e2b8fc109fc2c875a254bc6c57ffe3c2
coldrain-f <intmain__@naver.com>
2023-01-16 01:33:56 +0900

    Feat: [#31] 단어 컨트롤러 스웨거 태그 적용

--- src/main/java/edu/coldrain/controller/WordApiController.java
+++ src/main/java/edu/coldrain/controller/WordApiController.java
@@ -4,6 +4,8 @@ import edu.coldrain.dto.WordCreateRequest;
 import edu.coldrain.dto.WordResponse;
 import edu.coldrain.dto.WordUpdateRequest;
 import edu.coldrain.service.WordService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,15 +16,15 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api")
+@RequiredArgsConstructor
+@Tag(name = "WORD", description = "Word Api Controller")
 public class WordApiController {
 
     private final WordService wordService;
 
-    /**
-     * 단어 등록
-     */
+    @Tag(name = "WORD")
+    @ApiOperation(value = "단어 등록 API", notes = "", authorizations = {})
     @PostMapping("/categories/{id}/words")
     @ResponseStatus(HttpStatus.CREATED)
     public Long create(@PathVariable("id") final Long categoryId,
@@ -30,35 +32,31 @@ public class WordApiController {
         return wordService.create(request, categoryId);
     }
 
-    /**
-     * 단어 수정
-     */
+    @Tag(name = "WORD")
+    @ApiOperation(value = "단어 수정 API", notes = "", authorizations = {})
     @PatchMapping("/words/{id}")
     public void update(@PathVariable("id") final Long wordId,
                        @Validated @RequestBody final WordUpdateRequest request) {
         wordService.update(wordId, request);
     }
 
-    /**
-     * 단어 삭제
-     */
+    @Tag(name = "WORD")
+    @ApiOperation(value = "단어 삭제 API", notes = "", authorizations = {})
     @DeleteMapping("/words/{id}")
     public void delete(@PathVariable("id") final Long wordId) {
         wordService.delete(wordId);
     }
 
-    /**
-     * 단어 목록 조회
-     */
+    @Tag(name = "WORD")
+    @ApiOperation(value = "단어 목록 조회 API", notes = "", authorizations = {})
     @GetMapping("/words")
     public Page<WordResponse> findAll(
             @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
         return wordService.findAllByQuerydsl(pageable);
     }
 
-    /**
-     * 특정 카테고리에 소속된 단어 목록 조회
-     */
+    @Tag(name = "WORD")
+    @ApiOperation(value = "특정 카테고리에 소속된 단어 목록 조회 API", notes = "", authorizations = {})
     @GetMapping("/categories/{id}/words")
     public Page<WordResponse> findAllByCategoryId(@PathVariable("id") final Long categoryId,
             @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
