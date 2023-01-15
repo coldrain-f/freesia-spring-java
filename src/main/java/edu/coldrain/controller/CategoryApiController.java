90bce677b39c88d5ec92f9660ad7b2b14cec9c26
coldrain-f <intmain__@naver.com>
2023-01-16 01:29:12 +0900

    Feat: [#31] 카테고리 컨트롤러 스웨거 태그 적용

--- src/main/java/edu/coldrain/controller/CategoryApiController.java
+++ src/main/java/edu/coldrain/controller/CategoryApiController.java
@@ -4,8 +4,9 @@ import edu.coldrain.dto.CategoryCreateRequest;
 import edu.coldrain.dto.CategoryResponse;
 import edu.coldrain.dto.CategoryUpdateRequest;
 import edu.coldrain.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,16 +16,15 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Slf4j
-@RequiredArgsConstructor
 @RequestMapping("/api")
+@RequiredArgsConstructor
+@Tag(name = "CATEGORY", description = "Category Api Controller")
 public class CategoryApiController {
 
     private final CategoryService categoryService;
 
-    /**
-     * 카테고리 등록
-     */
+    @Tag(name = "CATEGORY")
+    @ApiOperation(value = "카테고리 등록 API", notes = "", authorizations = {})
     @PostMapping("/books/{id}/categories")
     @ResponseStatus(HttpStatus.CREATED)
     public Long create(@PathVariable("id") final Long bookId,
@@ -32,35 +32,31 @@ public class CategoryApiController {
         return categoryService.create(bookId, request);
     }
 
-    /**
-     * 카테고리 수정
-     */
+    @Tag(name = "CATEGORY")
+    @ApiOperation(value = "카테고리 수정 API", notes = "", authorizations = {})
     @PatchMapping("/categories/{id}")
     public void update(final @PathVariable("id") Long categoryId,
                        @Validated @RequestBody final CategoryUpdateRequest request) {
         categoryService.update(request, categoryId);
     }
 
-    /**
-     * 카테고리 삭제
-     */
+    @Tag(name = "CATEGORY")
+    @ApiOperation(value = "카테고리 삭제 API", notes = "", authorizations = {})
     @DeleteMapping("/categories/{id}")
     public void delete(final @PathVariable("id") Long categoryId) {
         categoryService.delete(categoryId);
     }
 
-    /**
-     * 카테고리 목록 조회
-     */
+    @Tag(name = "CATEGORY")
+    @ApiOperation(value = "카테고리 목록 조회 API", notes = "", authorizations = {})
     @GetMapping("/categories")
     public Page<CategoryResponse> findAllByQuerydsl(
             @PageableDefault(size = 5, sort = "createdAt", direction = Sort.Direction.DESC) final Pageable pageable) {
         return categoryService.findAllByQuerydsl(pageable);
     }
 
-    /**
-     * 특정 단어장에 소속된 모든 카테고리 조회
-     */
+    @Tag(name = "CATEGORY")
+    @ApiOperation(value = "특정 단어장에 소속된 카테고리 목록 조회 API", notes = "", authorizations = {})
     @GetMapping("/books/{id}/categories")
     public Page<CategoryResponse> findAllByBookId(
             @PathVariable("id") final Long bookId,
