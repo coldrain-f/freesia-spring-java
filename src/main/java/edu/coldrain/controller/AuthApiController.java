81b8cd5094851a06b09d14c22ddc7ebcf5c10dbc
coldrain-f <intmain__@naver.com>
2023-01-15 20:35:51 +0900

    Feat: [#28] JWT 로그인 API 기능 구현

---
+++ src/main/java/edu/coldrain/controller/AuthApiController.java
@@ -0,0 +1,45 @@
+package edu.coldrain.controller;
+
+import edu.coldrain.dto.TokenResponse;
+import edu.coldrain.dto.UserLoginRequest;
+import edu.coldrain.jwt.JwtFilter;
+import edu.coldrain.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthApiController {
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenResponse> authorize(@Valid @RequestBody UserLoginRequest request) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwtToken = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwtToken);
+
+        return new ResponseEntity<>(new TokenResponse(jwtToken), httpHeaders, HttpStatus.OK);
+    }
+}
