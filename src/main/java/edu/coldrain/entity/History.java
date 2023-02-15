d7a5b11ceeb674bebfad7477d6473450d07cce9b
coldrain-f <intmain__@naver.com>
2023-02-15 23:24:37 +0900

    Feat: [#41] 학습 기록 Entity 생성자와 변경 메서드 추가

--- src/main/java/edu/coldrain/entity/History.java
+++ src/main/java/edu/coldrain/entity/History.java
@@ -2,6 +2,7 @@ package edu.coldrain.entity;
 
 import edu.coldrain.common.Timestamped;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,4 +35,34 @@ public class History extends Timestamped {
 
     @Column
     private LocalDateTime completedAt;
+
+    @Builder
+    public History(final User user, final Category category, final Long numberOfReads, final LocalDateTime progressTime,
+                   final LocalDateTime completedAt) {
+        this.user = user;
+        this.category = category;
+        this.numberOfReads = numberOfReads;
+        this.progressTime = progressTime;
+        this.completedAt = completedAt;
+    }
+
+    public void changeUser(final User user) {
+        this.user = user;
+    }
+
+    public void changeCategory(final Category category) {
+        this.category = category;
+    }
+
+    public void changeNumberOfReads(final Long numberOfReads) {
+        this.numberOfReads = numberOfReads;
+    }
+
+    public void changeProgressTime(final LocalDateTime progressTime) {
+        this.progressTime = progressTime;
+    }
+
+    public void changeCompletedAt(final LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
 }
